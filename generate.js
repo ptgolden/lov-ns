399745bb7b3d1c3a80d83738cb438bdb154c0e97
Patrick Golden <ptgolden0@gmail.com>
2017-01-02 16:30:12 -0500

    Initial stab at it

---
+++ generate.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const fs = require('fs')
+    , path = require('path')
+    , fetch = require('node-fetch')
+    , mkdirp = require('mkdirp')
+
+const LOV_URL = 'http://lov.okfn.org/dataset/lov/api/v2/vocabulary/list'
+
+console.log(`Updating from ${LOV_URL}`)
+
+function getUpdatedList() {
+  return fetch(LOV_URL)
+    .then(res => {
+      if (!res.ok) {
+        console.log(`Error fetching LOV list (${res.status})`);
+        console.log(res.statusText);
+        process.exit(1);
+      }
+
+      return res.json();
+    })
+    .then(list => {
+      list.forEach(vocab => {
+        const { prefix, nsp } = vocab
+
+        if (!prefix || !nsp) {
+          console.log('Error getting prefix from item in LOV list.');
+          console.log('The format of the dataset has changed. Cannot continue.');
+          console.log('The format of the dataset may have changed.');
+        }
+
+        mkdirp.sync(prefix);
+
+        fs.writeFileSync(
+          path.join(prefix, 'vocab.json'),
+          JSON.stringify(vocab) + '\n'
+        );
+
+        fs.writeFileSync(
+          path.join(prefix, 'index.js'),
+          "module.exports = require('./vocab.json').nsp;\n"
+        );
+      })
+    })
+    .catch(err => {
+      throw err;
+    })
+}
+
+getUpdatedList();
