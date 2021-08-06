07416d34ba8d57838ec30e14180588c3abc85b56
Patrick Golden <ptgolden0@gmail.com>
2021-08-06 17:59:59 -0400

    Fix get_ns command to work with new module format

--- get_ns.js
+++ get_ns.js
@@ -1,16 +1,24 @@
 #!/usr/bin/env node
 
-"use strict";
-
 const prefix = process.argv[2];
 
 if (!/\w+/.test(prefix)) {
   process.exit(1);
 }
 
-try {
-  const ns = require('./' + prefix);
-  console.log(ns);
-} catch (e) {
-  process.exit(1);
+async function main() {
+  try {
+    const resp = await import(`./esm/${prefix}.js`)
+
+    console.log(resp.default)
+  } catch (e) {
+    if (e.message.startsWith('Cannot find module')) {
+      console.error(`Prefix \`${prefix}\` does not exist`)
+    } else {
+      console.error(e)
+    }
+    process.exit(1);
+  }
 }
+
+main()
